71aa539ca7b41bdd0f9425be92351bbcb23582e6
Mingwei Samuel <mingwei.samuel@gmail.com>
2023-01-07 21:15:40 -0800

    Update npm audit

--- test.bash
+++ test.bash
@@ -4,4 +4,4 @@ set -xe
 bash build.bash
 
 # TS_NODE_COMPILER_OPTIONS='{"include":["src/internal.semaphore.ts"]}'
-TS_NODE_SKIP_PROJECT=true node node_modules/mocha/bin/mocha
+TS_NODE_SKIP_PROJECT=true node_modules/.bin/mocha
